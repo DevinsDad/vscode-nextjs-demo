e704dddb91d336c9fa80cda1d92f5844586e6152
Tom Foran <ThomasForan@gmail.com>
2020-04-16 22:10:21 -0400

    Another version

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 function HomePage() {
     return <div>Welcome to Next.js! 
         Multiple Lines? 
+        Multiple Lines? 
+        Multiple Lines? 
+        Multiple Lines? 
+        Multiple Lines? 
     </div>; 
   }
   
